016463116bcb36318262d64c3e9c359c3e4d6ae0
Dennis de Jong <dennis.de.jong@navara.nl>
2025-07-28 15:36:22 +0200

    Fix initial

--- test_mars_rover.py
+++ test_mars_rover.py
@@ -3,7 +3,7 @@ from mars_rover import MarsRover
 
 class TestMarsRover(unittest.TestCase):
     def setUp(self):
-        self.rover = MarsRover()
+        self.rover = MarsRover((0,0), 'N')
 
     def test_initial_position(self):
         self.assertEqual(self.rover.position, (0, 0))
