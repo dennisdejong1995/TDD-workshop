8f3c7aacc09c0ae11efc06ef9b2f9d36e7b203e2
Dennis de Jong <dennis.de.jong@navara.nl>
2025-07-28 15:51:16 +0200

    Minimal example

--- mars_rover.py
+++ mars_rover.py
@@ -1,2 +1,15 @@
 class MarsRover:
-    pass
+    def __init__(self, position, direction):
+        self.position = (position[0], position[1])
+        self.direction = direction
+
+    def execute(self, command):
+        for move in command:
+            if move == 'f':
+                self.position = (self.position[0]+1, self.position[1])
+            elif move == 'b':
+                self.position = (self.position[0]-1, self.position[1])
+            elif move == 'l':
+                self.position = (self.position[0], self.position[1]-1)
+            elif move == 'r':
+                self.position = (self.position[0], self.position[1]+1)
--- test_mars_rover.py
+++ test_mars_rover.py
@@ -8,3 +8,32 @@ class TestMarsRover(unittest.TestCase):
     def test_initial_position(self):
         self.assertEqual(self.rover.position, (0, 0))
         self.assertEqual(self.rover.direction, 'N')
+
+    def test_receives_command(self):
+        command = 'fblr'
+        self.rover.execute(command)
+
+    def test_goes_forward(self):
+        command = 'f'
+        self.rover.execute(command)
+        self.assertEqual(self.rover.position, (1, 0))
+
+    def test_goes_backward(self):
+        command = 'b'
+        self.rover.execute(command)
+        self.assertEqual(self.rover.position, (-1, 0))
+
+    def test_goes_forward_then_backward(self):
+        command = 'fbb'
+        self.rover.execute(command)
+        self.assertEqual(self.rover.position, (-1, 0))
+
+    def test_goes_left(self):
+        command = 'l'
+        self.rover.execute(command)
+        self.assertEqual(self.rover.position, (0, -1))
+
+    def test_goes_right(self):
+        command = 'r'
+        self.rover.execute(command)
+        self.assertEqual(self.rover.position, (0, 1))
